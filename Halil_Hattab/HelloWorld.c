81bc2084d15e824cde8a4978bbe8df2e562c9e53
halil <socialmediaoftrs1342@gmail.com>
2025-10-30 22:12:38 +0300

    Halil_Hattab/ klasoru eklendi

---
+++ Halil_Hattab/HelloWorld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// printf(""); ile istenilen degeri yazdirabilirsiniz.
+
+int int main()
+{
+	printf("Hello World\n");
+	printf("Merhaba Dunya\n");
+	return 0;
+}
---
+++ Halil_Hattab/README.md
@@ -0,0 +1,2 @@
+# Halil Hattab
+- bu klasör [Halil Hattab (trs-1342)](https://github.com/trs-1342) tarafından geliştirilmiştir.
