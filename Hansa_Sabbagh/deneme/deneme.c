c1d559c691c1baea64f60739fd7e1dd9f1b17f08
Hansa_Sabbagh <hansa.sabbagh@org.gelisim.edu.tr>
2025-12-03 13:07:07 +0300

    tarif 1

--- Hansa_Sabbagh/deneme/deneme.c
+++ Hansa_Sabbagh/deneme/deneme.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main() {
-    printf("Hello, World!\n");
+    printf("Hello, World1342!\n");
     return 0;
 }
 // This code is written in C language standard version.
